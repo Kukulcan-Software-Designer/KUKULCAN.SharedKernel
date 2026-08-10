a3415c34b782cc967f0f3729055d6fe000f1d9fe
Juan Manuel Pardo García <jpardo.kukulcan@gmail.com>
2026-08-10 17:28:22 +0200

    Corrección de problemas en Enumeration "private static readonly ConcurrentDictionary<Type, IReadOnlyCollection> _cache = new();" y sobre todo en .Select(f => (Enumeration)f.GetValue(null)!). La colección que se construye es una colección de Enumeration, no de T. Después se intenta hacer "(IReadOnlyCollection<T>)..." y eso provoca exactamente el InvalidCastException que hemos visto.

--- Source/KUKULCAN.SharedKernel/Domain/Enumeration.cs
+++ Source/KUKULCAN.SharedKernel/Domain/Enumeration.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Linq;
 using System.Reflection;
 
 namespace KUKULCAN.SharedKernel.Domain;
@@ -7,9 +5,9 @@ namespace KUKULCAN.SharedKernel.Domain;
 /// <summary>
 /// Represents a rich enumeration.
 /// </summary>
-public abstract class Enumeration : IComparable, IComparable<Enumeration>
+public abstract class Enumeration : IComparable
 {
-    private static readonly ConcurrentDictionary<Type, IReadOnlyCollection<Enumeration>> _cache = new();
+    private static readonly ConcurrentDictionary<Type, object> _cache = new();
 
     /// <summary>
     /// Initializes a new enumeration.
@@ -36,37 +34,61 @@ public abstract class Enumeration : IComparable, IComparable<Enumeration>
     /// <summary>
     /// Returns every declared value of the enumeration.
     /// </summary>
-    public static IReadOnlyCollection<T> GetAll<T>() where T : Enumeration
+    /// <typeparam name="T">The enumeration type.</typeparam>
+    /// <returns>
+    /// All public static enumeration values declared by <typeparamref name="T"/>,
+    /// ordered by identifier.
+    /// </returns>
+    public static IReadOnlyCollection<T> GetAll<T>()
+        where T : Enumeration
     {
         return (IReadOnlyCollection<T>)_cache.GetOrAdd(
             typeof(T),
-            static type =>
-            {
-                return
-                [
-                    .. type
-                        .GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly)
-                        .Where(f => f.FieldType == type)
-                        .Select(f => (Enumeration)f.GetValue(null)!)
-                        .OrderBy(e => e.Id)
-                ];
-            });
+            static _ => CreateValues<T>());
     }
 
-    /// <inheritdoc/>
+    private static IReadOnlyCollection<T> CreateValues<T>()
+        where T : Enumeration
+    {
+        return
+        [
+            .. typeof(T)
+                .GetFields(
+                    BindingFlags.Public |
+                    BindingFlags.Static |
+                    BindingFlags.DeclaredOnly)
+                .Where(field => field.FieldType == typeof(T))
+                .Select(field => (T)field.GetValue(null)!)
+                .OrderBy(value => value.Id)
+        ];
+    }
+
+    /// <inheritdoc />
     public int CompareTo(object? obj)
         => CompareTo(obj as Enumeration);
 
-    /// <inheritdoc/>
-    public int CompareTo(Enumeration? other)
-    {
-        return other is null ? 1 : Id.CompareTo(other.Id);
-    }
+        /// <summary>
+        /// Compares this enumeration instance with another enumeration by identifier.
+        /// </summary>
+        /// <param name="other">
+        /// The enumeration instance to compare with the current instance.
+        /// </param>
+        /// <returns>
+        /// A value less than zero if the current identifier is less than
+        /// <paramref name="other"/>'s identifier; zero if they are equal; or a value
+        /// greater than zero if the current identifier is greater.
+        /// Returns <c>1</c> when <paramref name="other"/> is <see langword="null"/>.
+        /// </returns>
+        public int CompareTo(Enumeration? other)
+        => other is null
+            ? 1
+            : Id.CompareTo(other.Id);
 
-    /// <inheritdoc/>
-    public sealed override string ToString() => Name;
+    /// <inheritdoc />
+    public sealed override string ToString()
+        => Name;
 
-    /// <inheritdoc/>
+    /// <inheritdoc />
     public sealed override bool Equals(object? obj)
     {
         return obj is Enumeration other &&
@@ -74,10 +96,17 @@ public abstract class Enumeration : IComparable, IComparable<Enumeration>
                Id == other.Id;
     }
 
-    /// <inheritdoc/>
-    public sealed override int GetHashCode() => HashCode.Combine(GetType(), Id);
+    /// <inheritdoc />
+    public sealed override int GetHashCode()
+        => HashCode.Combine(GetType(), Id);
 
-    public static bool operator ==(Enumeration? left, Enumeration? right) => Equals(left, right);
+    public static bool operator ==(
+        Enumeration? left,
+        Enumeration? right)
+        => Equals(left, right);
 
-    public static bool operator !=(Enumeration? left, Enumeration? right) => !Equals(left, right);
+    public static bool operator !=(
+        Enumeration? left,
+        Enumeration? right)
+        => !Equals(left, right);
 }
